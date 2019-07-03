18ae8a966898df6e8aaf1f0b3b53aeaff1961a72
LuMengli <mengfaluo@163.com>
2019-07-03 15:40:15 +0800

    修改element引入方式

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
 import Vue from 'vue'
-
-import {
-  Table, TableColumn, Link, Input, Button, Select,
-} from 'element-ui'
-
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import './plugins/element.js'
 
 import 'assets/styles/index.styl'
-
-Vue.use(Table)
-Vue.use(TableColumn)
-Vue.use(Link)
-Vue.use(Input)
-Vue.use(Button)
-Vue.use(Select)
 
 Vue.config.productionTip = false
 
