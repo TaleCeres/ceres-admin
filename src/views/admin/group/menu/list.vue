1533dbc6991205eb1fcfbcec2a98416d222bd6e5
LuMengli <mengfaluo@163.com>
2020-05-11 10:55:18 +0800

    feat: 菜单新增删除和拖拽

--- src/views/admin/group/menu/list.vue
+++ src/views/admin/group/menu/list.vue
@@ -1,105 +1,183 @@
 <template>
   <div class="container">
-    <div class="block">
-      <el-button type="botton" size="mini" @click="appendToRoot(data)">
-        新增根节点
-      </el-button>
-      <el-tree :data="data" node-key="id" draggable default-expand-all :expand-on-click-node="false">
-        <div slot-scope="{ node, data }" class="custom-tree-node">
-          <span>{{ node.label }} {{data.name}}</span>
-          <el-button type="text" size="mini" @click="edit(node, data)">
-            编辑
-          </el-button>
-          <el-button type="text" size="mini" @click="append(data)">
-            添加
-          </el-button>
-          <el-button type="text" size="mini" @click="remove(node, data)">
-            删除
-          </el-button>
-        </div>
-      </el-tree>
-    </div>
+    <el-row :gutter="20" style="padding: 20px 0">
+      <el-col :span="12">
+        <el-card>
+          <div slot="header">
+            <el-button size="medium" type="primary" @click="updateRouteTree">保存</el-button>
+            <el-button size="medium" @click="getRouteTree">刷新</el-button>
+            <el-button size="medium" style="float: right" :disabled="dragFlag" @click="dialogAddVisible = true">新增菜单</el-button>
+          </div>
+          <div class="block">
+            <el-alert
+              v-show="dragFlag"
+              title="菜单拖拽后请先保存或刷新再进行其他操作"
+              :closable="false"
+              type="info">
+            </el-alert>
+            <el-tree
+              :data="routeTree"
+              node-key="id"
+              draggable
+              default-expand-all
+              :expand-on-click-node="false"
+              @node-drop="handleDrop">
+              <div slot-scope="{ node, data }" class="custom-tree-node">
+                <i :class="data.icon"></i>
+                <span>{{ data.title }}</span>
+                <router-link class="path" :to="data.path.toString()">{{data.path}}</router-link>
+                <div class="btn">
+                  <el-button type="text" size="mini" :disabled="dragFlag" @click="edit(node, data)">
+                    编辑
+                  </el-button>
+                  <el-button type="text" size="mini" :disabled="dragFlag" @click="remove(node, data)">
+                    删除
+                  </el-button>
+                </div>
+              </div>
+            </el-tree>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <el-dialog
+      title="新建菜单"
+      :visible.sync="dialogAddVisible"
+      width="600px">
+      <el-form ref="menuForm" :model="form" label-width="80px">
+        <el-form-item label="父级">
+          <treeselect
+            v-model="form.parent_id"
+            :normalizer="normalizer"
+            :options="menuOptions"
+            :show-count="true"
+            placeholder="选择上级菜单"/>
+        </el-form-item>
+        <el-form-item label="菜单名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="Title">
+          <el-input v-model="form.title"></el-input>
+        </el-form-item>
+        <el-form-item label="路径">
+          <el-input v-model="form.path"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-popover
+            placement="bottom-start"
+            width="460"
+            trigger="click"
+            @show="$refs['iconSelect'].reset()"
+          >
+            <IconSelect ref="iconSelect" @selected="selected" />
+            <el-input slot="reference" v-model="form.icon" placeholder="点击选择图标" readonly>
+              <i
+                v-if="form.icon"
+                slot="prepend"
+                :class="form.icon"
+                class="el-input__icon"
+                style="height: 32px;width: 16px;"
+              />
+              <i v-else slot="prepend" class="el-icon-search el-input__icon" />
+            </el-input>
+          </el-popover>
+        </el-form-item>
+        <el-form-item label="组件路径">
+          <el-input v-model="form.component"></el-input>
+        </el-form-item>
+        <el-form-item label="菜单状态">
+          <el-radio-group v-model="form.hidden">
+            <el-radio :label="true">隐藏</el-radio>
+            <el-radio :label="false">显示</el-radio>
+          </el-radio-group>
+        </el-form-item>
+<!--        <el-form-item>-->
+<!--          <el-button @click="closeDialog">取消</el-button>-->
+<!--          <el-button type="primary" @click="createRoute">确定</el-button>-->
+<!--        </el-form-item>-->
+      </el-form>
+      <span slot="footer">
+        <el-button @click="closeDialog">取消</el-button>
+        <el-button type="primary" @click="createRoute">确定</el-button>
+  </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+// import the component
+import Treeselect from '@riophae/vue-treeselect'
+// import the styles
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+import RouteModel from '@/models/route'
+
+import IconSelect from '@/components/base/IconSelect'
+
 let id = 1000
 
 export default {
   name: 'GroupMenuList',
   components: {
+    Treeselect,
+    IconSelect,
   },
   data() {
-    const data = [{
-      id: 1,
-      label: '系统管理',
-      name: 'Admin',
-      path: '/admin',
-      components: undefined,
-      icon: ['el-icon-user'],
-      children: [{
-        id: 2,
-        label: '用户管理',
-        name: 'AdminUser',
-        path: '/admin/user',
-        component: undefined,
-        icon: ['el-icon-house'],
-        children: [{
-          id: 3,
-          label: '用户列表',
-          name: 'UserList',
-          path: '/admin/user/list',
-          component: 'admin/user/list',
-          icon: ['el-icon-collection-tag'],
-        }]
-      }, {
-        id: 4,
-        label: '权限组管理',
-        name: 'AdminGroup',
-        path: '/admin/group',
-        component: undefined,
-        icon: ['fa', 'fa-group'],
-        children: [{
-          id: 5,
-          label: '接口权限',
-          name: 'GroupApiList',
-          path: '/admin/group/api',
-          component: 'admin/group/api/list',
-          icon: ['el-icon-collection-tag'],
-        }, {
-          id: 6,
-          label: '页面权限',
-          name: 'GroupPageList',
-          path: '/admin/group/page',
-          component: 'admin/group/page/list',
-          icon: ['el-icon-collection-tag'],
-        }]
-      }]
-    }, {
-      id: 7,
-      label: '类别管理',
-      name: 'Category',
-      path: '/category',
-      component: undefined,
-      children: [{
-        id: 8,
-        label: '类别列表',
-        name: 'CategoryList',
-        path: '/category/list',
-        component: 'category/list',
-        icon: ['el-icon-house'],
-      }
-      ]
-    }]
     return {
-      data: JSON.parse(JSON.stringify(data))
+      routeTree: [],
+      menuOptions: [],
+      form: {
+        component: undefined,
+        hidden: false,
+        icon: '',
+        name: '',
+        parent_id: 0,
+        title: ''
+      },
+      dragFlag: false,
+      dialogAddVisible: false
     }
   },
+  computed: {
+  },
+  mounted() {
+    this.getRouteTree()
+  },
   methods: {
-    appendToRoot(data) {
-      id += 1
-      const newChild = { id, label: 'test', children: [] }
-      data.push(newChild)
+    // 选择图标
+    selected(name) {
+      this.form.icon = `fa ${name}`
+    },
+    async getRouteTree() {
+      try {
+        const res = await RouteModel.getRouteTree()
+        this.routeTree = [...res]
+        this.getTreeSelect()
+        this.dragFlag = false
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    // 树形选择器结构
+    async getTreeSelect() {
+      this.menuOptions = []
+      const res = await RouteModel.getRouteTree()
+      const menu = { id: 0, title: '主类目', children: [] }
+      menu.children = [...res]
+      this.menuOptions.push(menu)
+    },
+    /** 转换菜单数据结构 */
+    normalizer(node) {
+      if (node.children && !node.children.length) {
+        delete node.children
+      }
+      return {
+        id: node.id,
+        label: node.title,
+        children: node.children
+      }
     },
     edit(note, data) {
       // 弹出浮层
@@ -115,24 +193,93 @@ export default {
       }
       data.children.push(newChild)
     },
+
+    // 删除菜单
     remove(node, data) {
-      const { parent } = node
-      const children = parent.data.children || parent.data
-      const index = children.findIndex(d => d.id === data.id)
-      children.splice(index, 1)
+      this.$confirm('此操作将永久删除该菜单及其子菜单, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        try {
+          await RouteModel.deleteRoute(data.id)
+          this.$message.success('删除菜单成功！')
+          this.getRouteTree()
+        } catch (e) {
+          console.log(e)
+        }
+      })
     },
+    // 新增路由
+    async createRoute() {
+      await RouteModel.createRoute(this.form)
+      this.$message.success('新增菜单成功')
+      this.resetForm()
+      this.getRouteTree()
+      this.dialogAddVisible = false
+    },
+    // 重置新增表单
+    resetForm() {
+      this.form = {
+        component: undefined,
+        hidden: false,
+        icon: '',
+        name: '',
+        parent_id: 0,
+        title: ''
+      }
+    },
+
+    // 拖拽完成时触发事件
+    handleDrop(draggingNode, dropNode, dropType, ev) {
+      this.dragFlag = true
+    },
+
+    closeDialog() {
+      this.dialogAddVisible = false
+      this.resetForm()
+    },
+    // 拖动后保存路由
+    async updateRouteTree() {
+      try {
+        await RouteModel.updateRoute(this.routeTree)
+        this.$message.success('菜单保存成功')
+        this.getRouteTree()
+      } catch (e) {
+        this.$message.error('菜单保存失败！')
+      }
+    }
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 .container {
-  width 600px
-  flex 1
-  display flex
-  align-items center
-  justify-content space-between
-  font-size 14px
-  padding-right 8px
+  min-width 900px
+  .block {
+    .custom-tree-node {
+      width 100%
+      line-height 44px
+      span {
+        margin-left 10px
+      }
+      .path {
+        color #1890ff
+        margin-left 10px
+      }
+      .btn {
+        float right !important
+      }
+    }
+  }
+}
+</style>
+<style lang="stylus" rel="stylesheet/stylus">
+.block {
+  .el-tree-node__content {
+    padding: 8px 10px
+    margin: 1px 0
+    border: 1px solid #ddd
+  }
 }
 </style>
