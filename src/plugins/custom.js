31a5be3e010779cace9a94177310ae5458c9ea38
Allen7D <bodanli159951@163.com>
2020-06-18 10:29:40 +0800

    refactor: 优化CeresTable的位置和导入方式

--- src/plugins/custom.js
+++ src/plugins/custom.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import FaIcon from 'comps/base/FaIcon'
-import CeresTable from 'comps/base/table/CeresTable'
+import CeresTable from 'comps/base/CeresTable'
 import color from 'assets/data/color'
 import model from '@/models'
 
@@ -10,7 +10,7 @@ Vue.prototype.$color = color
 Vue.prototype.$model = model // 将model挂载到vue的原型上
 if (process.env.NODE_ENV !== 'production') {
   // mock数据的接口
-  // eslint-disable-next-line 
+  // eslint-disable-next-line
   const Mock = require('../../mock')
   Vue.prototype.$mockApi = Mock.default.$mockApi
 }
